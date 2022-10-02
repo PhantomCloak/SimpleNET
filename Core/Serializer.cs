c696f2f2f829587734ee76e5bdedf03fe2aa3236
PhantomCloak <phantom_clk@protonmail.com>
2022-10-02 03:17:01 +0300

    add: string support

--- Core/Serializer.cs
+++ Core/Serializer.cs
@@ -1,4 +1,6 @@
 
+using System.Text;
+
 namespace SimpleNET
 {
     public class Serializer
@@ -24,6 +26,13 @@ namespace SimpleNET
             return value;
         }
 
+        public static string ReadString(byte[] buffer, ref int readIndex)
+        {
+            var len = ReadInt(buffer, ref readIndex);
+            var value = Encoding.ASCII.GetString(buffer, readIndex, len);
+            return value;
+        }
+
         public static void WriteInt(int value, ref byte[] buffer, ref int writeIndex)
         {
             var valBuf = BitConverter.GetBytes(value);
@@ -43,6 +52,14 @@ namespace SimpleNET
             var valBuf = BitConverter.GetBytes(value);
             Buffer.BlockCopy(valBuf, 0, buffer, writeIndex, sizeof(float));
             writeIndex += sizeof(float);
+        }
+
+        public static void WriteString(string value, ref byte[] buffer, ref int writeIndex)
+        {
+            WriteInt(value.Length, ref buffer, ref writeIndex);
+            var valBuf = Encoding.ASCII.GetBytes(value);
+            Buffer.BlockCopy(valBuf, 0, buffer, writeIndex, valBuf.Length);
+            writeIndex += valBuf.Length;
         }
     }
 }
