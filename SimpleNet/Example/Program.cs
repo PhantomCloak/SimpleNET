36e9a7fe18b9549dfa3218d45b83efde7da007e7
PhantomCloak <phantom_clk@protonmail.com>
2022-10-03 21:12:04 +0300

    add: explicit usings

--- SimpleNet/Example/Program.cs
+++ SimpleNet/Example/Program.cs
@@ -1,8 +1,8 @@
-﻿
-using System.Diagnostics;
-using System.Net;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Sockets;
-using System.Text;
+using System.Threading;
 using SimpleNET;
 
 namespace MyApp // Note: actual namespace depends on the project name.
